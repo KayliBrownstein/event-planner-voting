0a0c23a21e5192bdf362e6a7a57f9e0e0ff2d470
KayliBrownstein <kaylivbrownstein@gmail.com>
2017-05-03 13:28:54 -0400

    datetime votes appear but dont immediately refresh

--- react/src/components/DatetimeTile.js
+++ react/src/components/DatetimeTile.js
@@ -4,6 +4,7 @@ const DatetimeTile = (props) => {
   return(
     <div className="small-12 medium-5 large-3 columns datetime-tile">
       <div className='datetime-box-content'>
+      <span><button className="button" onClick={props.upvoteHandler}>+</button></span><span>{props.votes}</span><span><button className="button" onClick={props.downvoteHandler}>-</button></span>
         <p>{props.date}</p>
         <p>{props.time}</p>
       </div>
