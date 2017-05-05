4372477d6dc3523c5ba7fa4d0794759a2778b9b8
KayliBrownstein <kaylivbrownstein@gmail.com>
2017-05-04 21:43:46 -0400

    css styling

--- react/src/components/NewEventForm.js
+++ react/src/components/NewEventForm.js
@@ -4,7 +4,7 @@ const NewEventForm = props => {
  return(
    <div className="column row submit-event">
      <div className= 'new-event-button'>
-       <center><button type="button" onClick={props.handleFormButtonClick} className='button create-event small-12 medium-10 large-6 medium-centered large-centered columns'>Create an Event</button></center>
+       <center><button type="button" onClick={props.handleFormButtonClick} className='button create-event small-12 medium-10 large-4 medium-centered large-centered columns'>Create an Event</button></center>
      </div>
      <form onSubmit={props.handleSubmit} className={props.className} id="new-event">
        <label> Name: </label>
