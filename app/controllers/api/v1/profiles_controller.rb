5bdc8881f085404784a8a2ca37ac0fddb8d46d10
KayliBrownstein <kaylivbrownstein@gmail.com>
2017-05-09 13:07:11 -0400

    modify config.compile.assets to true in production.rb

--- app/controllers/api/v1/profiles_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::ProfilesController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
-  def index
-    @events = Event.where(user_id: current_user.id)
-    @current_user = current_user
-    render json: @current_user
-  end
-end
