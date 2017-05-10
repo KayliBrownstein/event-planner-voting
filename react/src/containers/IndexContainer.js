c77aa0607eedd7cb46ef09196987ac3ced0e972b
KayliBrownstein <kaylivbrownstein@gmail.com>
2017-05-10 13:22:39 -0400

    minor fixes done for now

--- react/src/containers/IndexContainer.js
+++ react/src/containers/IndexContainer.js
@@ -169,7 +169,7 @@ class IndexContainer extends Component {
 
            handleSubmit = {this.handleSubmit}
          />
-        <div className='column row'>
+        <div className='column row all-events-target'>
          <AllEvents
             events={this.state.events}
           />
