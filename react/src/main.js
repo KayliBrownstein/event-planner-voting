958aca4c3743c7aa0f8cbed35b3cca9f5651abbe
KayliBrownstein <kaylivbrownstein@gmail.com>
2017-05-11 15:33:50 -0400

    imported npm package in main.js

--- react/src/main.js
+++ react/src/main.js
@@ -2,6 +2,7 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
+import 'whatwg-fetch';
 
 $(function() {
   if (document.getElementById('app')) {
